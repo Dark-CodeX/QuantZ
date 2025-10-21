02331101c36322bc92a165ff27cc8417352cdb7c
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2025-10-21 19:00:28 +0530

    UPDATE: 1. using CORS 2. created working version of all indicators

--- src/ml/api.py
+++ src/ml/api.py
@@ -1,6 +1,8 @@
 from io import StringIO
 from flask import Flask, request, jsonify
+import json
 import pandas as pd
+from flask_cors import CORS
 import quantzlib as qz
 
 global_df = None  # better to use None
@@ -30,6 +32,7 @@ def CleanCSV(data):
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/upload/<type>", methods=["POST"])
@@ -49,17 +52,56 @@ def indicators(indicator):
         return "No data loaded. Upload CSV first.", 400
 
     data = request.get_json(force=True)
-    period = data.get("period")
     if indicator == "SMA":
+        period = data.get("period")
         return str(qz.SMA(global_df["close"], period))
     elif indicator == "EMA":
+        period = data.get("period")
         return str(qz.EMA(global_df["close"], period))
     elif indicator == "RSI":
+        period = data.get("period")
         return str(qz.RSI(global_df["close"], period))
     elif indicator == "ATR":
+        period = data.get("period")
         return str(qz.ATR(global_df["high"], global_df["low"], global_df["close"], period))
+    elif indicator == "MACD":
+        fast = data.get("fast")
+        slow = data.get("slow")
+        return str(qz.MACD(global_df["close"], fast, slow))
+    elif indicator == "VWMA":
+        period = data.get("period")
+        return str(qz.VWMA(global_df["close"], global_df["volume"], period))
+    elif indicator == "BollingerBands":
+        period = data.get("period")
+        multiplier = data.get("multiplier")
+        return str(qz.BollingerBands(global_df["close"], period, multiplier))
+    elif indicator == "WMA":
+        period = data.get("period")
+        w = data.get("weights").lower()
+        if w == "linear":
+            weights = [i for i in range(1, period + 1)]
+        elif w == "normalized linear":
+            weights = [i/period for i in range(1, period + 1)]
+        elif w == "harmonic":
+            weights = [1/i for i in range(1, period + 1)]
+        elif w == "triangular":
+            mid = (period + 1) // 2
+            if period % 2 == 0:
+                weights = list(range(1, mid + 1)) + list(range(mid, 0, -1))
+            else:
+                weights = list(range(1, mid + 1)) + list(range(mid - 1, 0, -1))
+        elif w == "quadratic":
+            weights = [i**2 for i in range(1, period + 1)]
+        elif w == "cubic":
+            weights = [i**3 for i in range(1, period + 1)]
+        elif w == "root":
+            weights = [i**0.5 for i in range(1, period + 1)]
+        else:
+            return f"Error: unknown WMA weight type '{w}'", 400
+
+        return str(qz.WMA(global_df["close"], weights, period))
     return f"Error: unknown indicator '{indicator}'", 400
 
 
 if __name__ == '__main__':
-    app.run(port=9080)
+    app.run(host='0.0.0.0', port=10000)
