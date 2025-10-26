4d2918b8d714625fcf78d9473a3ff42686a4e453
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2025-10-26 18:40:56 +0530

    UPDATE: 1. global ErrorBox instead of domain specific

--- src/web/src/App.jsx
+++ src/web/src/App.jsx
@@ -1,19 +1,23 @@
+import { useState } from "react";
 import Home from './components/Home'
 import Main from './components/Main';
 import Navbar from "./components/Navbar"
-import { useState } from "react";
+import ErrorBox from "./components/ErrorBox";
 
 function App() {
     const [file, setFile] = useState(null);
     const [CSVData, setCSVData] = useState([]);
-
+    const [errorMessage, setErrorMessage] = useState([]);
     return (
         <>
             <Navbar />
             {!file ? (
                 <Home file={file} setFile={setFile} setCSVData={setCSVData} />
             ) : (
-                <Main CSVData={CSVData} />
+                <Main CSVData={CSVData} setErrorMessage={setErrorMessage} />
+            )}
+            {errorMessage && (
+                <ErrorBox errorMessage={errorMessage} setErrorMessage={setErrorMessage} />
             )}
         </>
     );
