07f6b87870144dff585117ba0725f56d817e27a5
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2025-10-26 18:41:44 +0530

    UPDATE: 1. Fixed ErrorBox at Bottom-RIght

--- src/web/src/components/ErrorBox.jsx
+++ src/web/src/components/ErrorBox.jsx
@@ -1,16 +1,25 @@
+import { useEffect } from "react";
 import "../css/root.css"
 import "../css/ErrorBox.css"
 import { LiveButton } from "./LiveUI";
 
-const ErrorBox = ({ msg, setErrorMessage }) => {
+const ErrorBox = ({ errorMessage, setErrorMessage }) => {
 
     const deleteItem = (id) => {
         setErrorMessage(prev => prev.filter((_, index) => index !== id));
     };
 
+    useEffect(() => {
+        if (!errorMessage.length) return;
+        const timer = setTimeout(() => {
+            setErrorMessage((prev) => prev.slice(1)); // remove first message immutably
+        }, 1500);
+        return () => clearTimeout(timer);
+    }, [errorMessage]);
+
     return (
         <div className="error-box-container-parent">
-            {msg.map((v, i) => {
+            {errorMessage.map((v, i) => {
                 return (
                     <div key={i} className="error-box-container">
                         <p>{v}</p>
